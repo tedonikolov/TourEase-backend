f4a004cc3a6860d3e1a6b42dc6b4614e03f3bf25
tedonikolov <teonikolov01@abv.bg>
2024-01-13 17:08:03 +0200

    Created logger_service module

---
+++ logger_service/src/main/java/com/tourease/logger/LoggerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tourease.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LoggerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerServiceApplication.class, args);
+    }
+
+}
---
+++ logger_service/src/test/java/com/tourease/logger_service/LoggerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourease.logger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
