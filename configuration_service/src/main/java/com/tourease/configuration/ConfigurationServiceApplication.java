bd99952ddb052baf77d48048c68cbe467e26490f
tedonikolov <teonikolov01@abv.bg>
2024-01-13 16:59:54 +0200

    Created configuration_service module

---
+++ configuration_service/src/main/java/com/tourease/configuration/ConfigurationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tourease.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+
+}
