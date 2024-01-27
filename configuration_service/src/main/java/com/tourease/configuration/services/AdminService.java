d188ebaa9f77b5e3b3d5964d03ac3e2b7bc026fa
tedonikolov <teonikolov01@abv.bg>
2024-01-27 17:00:54 +0200

    Added kafka producer for logging

--- configuration_service/src/main/java/com/tourease/configuration/services/AdminService.java
+++ configuration_service/src/main/java/com/tourease/configuration/services/AdminService.java
@@ -5,6 +5,7 @@ import com.tourease.configuration.models.entities.Configuration;
 import com.tourease.configuration.models.enums.Field;
 import com.tourease.configuration.repositories.ConfigurationRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,12 +14,14 @@ import java.util.List;
 @AllArgsConstructor
 public class AdminService {
     private final ConfigurationRepository configurationRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void save(List<UpdateConfigurationDTO> configurationsVO) {
         for(UpdateConfigurationDTO updateConfiguration:configurationsVO){
             Configuration configuration = configurationRepository.findByName(Field.valueOfLabel(updateConfiguration.getName()));
             configuration.setValue(updateConfiguration.getValue());
             configurationRepository.save(configuration);
+            kafkaTemplate.send("configuration_service","admin","Configurations uppdated!");
         }
     }
 }
