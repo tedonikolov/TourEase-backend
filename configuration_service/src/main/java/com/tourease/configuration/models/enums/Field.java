5c1146f2790a45c255a9e310eea57c84cccb9f19
tedonikolov <teonikolov01@abv.bg>
2024-01-27 11:56:21 +0200

    Created Email system Configuration

---
+++ configuration_service/src/main/java/com/tourease/configuration/models/enums/Field.java
@@ -0,0 +1,35 @@
+package com.tourease.configuration.models.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Field {
+    EMAIL_FROM("emailFrom"),
+    EMAIL_PASSWORD("emailPassword");
+
+    private final String label;
+    Field(String label) {
+        this.label = label;
+    }
+
+    private static final Map<String, Field> BY_LABEL = new HashMap<>();
+
+    static {
+        for (Field e : values()) {
+            BY_LABEL.put(e.label.toUpperCase(), e);
+        }
+    }
+
+    public static Field valueOfLabel(String label) {
+        if (label == null) return null;
+        Field field = BY_LABEL.get(label.toUpperCase());
+        if (field == null)
+            for (Field value : values()) {
+                if (label.toUpperCase().equals(value.toString())) {
+                    return value;
+                }
+            }
+
+        return field;
+    }
+}
