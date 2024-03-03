b218b9128b4b211437ddd6d97fd5eb4ea6023649
tedonikolov <teonikolov01@abv.bg>
2024-03-03 21:11:05 +0200

    Fixed CustomExceptionHandler

--- configuration_service/src/main/java/com/tourease/configuration/exception/CustomExceptionHandler.java
+++ configuration_service/src/main/java/com/tourease/configuration/exception/CustomExceptionHandler.java
@@ -2,11 +2,36 @@ package com.tourease.configuration.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        List<ValidationError> list = new ArrayList<>();
+        for (ObjectError error :  ex.getBindingResult().getAllErrors()) {
+            String fieldName;
+            if(error instanceof FieldError)
+                fieldName = ((FieldError) error).getField();
+            else
+                fieldName = error.getObjectName();
+            String errorMessage = error.getDefaultMessage();
+            list.add(new ValidationError(fieldName, errorMessage, ErrorCode.Failed));
+        }
+        ErrorResponse response = new ErrorResponse("The request could not be executed", list);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
     @ExceptionHandler()
     public ResponseEntity<String> handleInternalExceptions(RuntimeException ex) {
         ex.printStackTrace();
--- hotel_service/src/main/java/com/tourease/hotel/controllers/expection/CustomExceptionHandler.java
+++ hotel_service/src/main/java/com/tourease/hotel/controllers/expection/CustomExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.tourease.user.controllers.exceptions;
+package com.tourease.hotel.controllers.expection;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
