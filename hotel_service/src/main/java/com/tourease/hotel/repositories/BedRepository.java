24d49f5a0a0a2c4f8b3f80fc3c886ed8c985c6c7
tedonikolov <teonikolov01@abv.bg>
2024-05-11 09:47:22 +0300

    Fixed room types beds save of same time

--- hotel_service/src/main/java/com/tourease/hotel/repositories/BedRepository.java
+++ hotel_service/src/main/java/com/tourease/hotel/repositories/BedRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BedRepository extends JpaRepository<Bed, Long> {
     @Query("select b from Bed b where b.name = ?1 and b.hotel.id = ?2")
     Optional<Bed> findByNameAndHotel_Id(String name, Long id);
+
+    @Query("select b from Bed b where b.hotel.id = ?1")
+    List<Bed> findByHotel_Id(Long id);
 }
